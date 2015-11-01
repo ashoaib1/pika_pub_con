09d183ca9dc08eabd249ad9275f43adf53ec9a44
Ahsan Shoaib <ashoaib.nas@gmail.com>
2015-10-31 23:00:11 -0400

    Retry connecting if channel is closed by broker node going down

--- consumer.py
+++ consumer.py
@@ -3,6 +3,7 @@
 import pika
 import logging
 from datetime import datetime
+from time import sleep
 
 logging.basicConfig(level=logging.INFO)
 
@@ -13,25 +14,32 @@ def process_message(channel, method_frame, header_frame, message):
 
 # Create connection and open channel
 credentials = pika.PlainCredentials(username='rmq_test', password='rmq_test')
-parameters =  pika.ConnectionParameters(host='rmqnode3', port=5672, virtual_host='/', credentials=credentials)
-connection = pika.BlockingConnection(parameters)
-channel = connection.channel()
+rmq_nodes = ['rmqnode1', 'rmqnode2', 'rmqnode3']
+while True:
+	for rmq_node in rmq_nodes:
+		sleep(2)
+		try:
+			parameters =  pika.ConnectionParameters(host=rmq_node, port=5672, virtual_host='/', credentials=credentials)
+			print "Trying to connect to RabbitMQ Node {}".format(rmq_node)
+			connection = pika.BlockingConnection(parameters)
+			channel = connection.channel()
 
-# Declare all resources
-exchange_name = 'test_exchange'
-queue_name = 'test_queue'
-routing_key = 'test_routing_key'
-channel.exchange_declare(exchange=exchange_name, exchange_type="direct", passive=False, durable=True, auto_delete=False)
-channel.queue_declare(queue=queue_name, durable=True)
-channel.queue_bind(queue=queue_name, exchange=exchange_name, routing_key=routing_key)
-channel.basic_qos(prefetch_count=10)
+			# Declare all resources
+			exchange_name = 'test_exchange'
+			queue_name = 'test_queue'
+			routing_key = 'test_routing_key'
+			channel.exchange_declare(exchange=exchange_name, exchange_type="direct", passive=False, durable=True, auto_delete=False)
+			channel.queue_declare(queue=queue_name, durable=True)
+			channel.queue_bind(queue=queue_name, exchange=exchange_name, routing_key=routing_key)
+			channel.basic_qos(prefetch_count=10)
 
-# Assign callback function to consume messages
-channel.basic_consume(consumer_callback=process_message, queue=queue_name)
+			# Assign callback function to consume messages
+			channel.basic_consume(consumer_callback=process_message, queue=queue_name)
 
-try:
-    channel.start_consuming()
-except KeyboardInterrupt:
-    channel.stop_consuming()
-
-connection.close()
+			try:
+			    channel.start_consuming()
+			except KeyboardInterrupt:
+			    channel.stop_consuming()
+			    connection.close()
+		except Exception as e:
+			print "Message consumption stopped due to exception [{}]".format(e)
